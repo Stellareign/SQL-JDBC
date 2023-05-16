2895794d5c676a20397817fdcacb6807a6db6e64
Stellareign <tuguran@mail.ru>
2023-05-16 22:15:36 +0300

    sql

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,4 @@
+import javax.sql.rowset.RowSetWarning;
 import java.sql.*;
 
 public class Application {
@@ -6,8 +7,18 @@ public class Application {
        final String password = "1234";
        final String url = "jdbc:postgresql://localhost:5432/skypro";
 
-        final Connection connection = DriverManager.getConnection(url, user, password);
+       try (Connection connection = DriverManager.getConnection(url, user, password);
+           Statement statement = connection.createStatement() // для запросов к БД
+                   ) {
+           ResultSet resultSet = statement.executeQuery("SELECT * FROM employee WHERE id = (1)");
+           while (resultSet.next()) {
+               System.out.println(resultSet);
 
-        PreparedStatement statement = connection.prepareStatement("SELECT * FROM employee WHERE id = (?)");
+               //   PreparedStatement statement1 = connection.prepareStatement("SELECT * FROM employee WHERE id = (?)"); // для запросов к базе
+
+           }
+       }catch (SQLException e){
+           throw new RuntimeException(e);
+       }
     }
 }
